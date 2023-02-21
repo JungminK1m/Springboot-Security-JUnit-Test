c8820f82e0da7d37bfc0a93f034d0368e5363279
unknown <deliah07@naver.com>
2023-02-21 16:33:57 +0900

    컨트롤러, 레파지토리 테스트 완료

---
+++ src/test/java/com/example/loginexample/controller/UserControllerTest.java
@@ -0,0 +1,81 @@
+package com.example.loginexample.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import com.example.loginexample.model.User;
+
+import javax.servlet.http.HttpSession;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/*
+ * SpringBootTest는 통합테스트 (실제 환경과 동일하게 Bean이 생성됨) 
+ * AutoConfigureMockMvc는 Mock 환경의 IoC 컨테이너에 MockMvc Bean이 생성됨
+ */
+
+@Transactional
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = WebEnvironment.MOCK)
+public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    private MockHttpSession mockSession;
+
+    @BeforeEach
+    public void setUp() {
+        mockSession = new MockHttpSession();
+        // User 객체 가짜로 만들기
+        User user = new User();
+        user.setUsername("ssar");
+        user.setPassword("1234");
+        user.setEmail("ssar@nate.com");
+        mockSession.setAttribute("principal", user);
+    }
+
+    @Test
+    public void join_test() throws Exception {
+        // given
+        String requestBody = "username=lala&password=1234&email=lala@nate.com";
+
+        // when
+        ResultActions resultActions = mvc.perform(post("/join").content(requestBody)
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE));
+
+        // then
+        resultActions.andExpect(status().is3xxRedirection());
+    }
+
+    @Test
+    public void login_test() throws Exception {
+        // given
+        String requestBody = "username=ssar&password=1234";
+
+        // when
+        ResultActions resultActions = mvc.perform(post("/login").content(requestBody)
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE));
+        HttpSession session = resultActions.andReturn().getRequest().getSession();
+        User principal = (User) session.getAttribute("principal");
+
+        System.out.println(principal.getUsername());
+        System.out.println(principal.getPassword());
+
+        // then
+        assertThat(principal.getUsername()).isEqualTo("ssar");
+        resultActions.andExpect(status().is3xxRedirection());
+    }
+}
---
+++ src/test/java/com/example/loginexample/model/UserRepositoryTest.java
@@ -0,0 +1,35 @@
+package com.example.loginexample.model;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@MybatisTest
+public class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void findAll_test() throws Exception {
+        // given
+        ObjectMapper om = new ObjectMapper(); // Jackson
+
+        // when
+        List<User> JoinReqDto = userRepository.findAll();
+        String responseBody = om.writeValueAsString(JoinReqDto);
+        System.out.println("테스트 : " + responseBody);
+
+        // then
+        assertThat(JoinReqDto.get(1).getUsername());
+        assertThat(JoinReqDto.get(1).getPassword());
+    }
+
+    private Object assertThat(String username) {
+        return null;
+    }
+}
