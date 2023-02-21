769676b3fa7d9fb5182d273e6bbfaeb08cafe3e8
unknown <deliah07@naver.com>
2023-02-21 09:46:08 +0900

    세팅 및 테스트 완료

---
+++ src/main/java/com/example/loginexample/Controller/TestController.java
@@ -0,0 +1,13 @@
+package com.example.loginexample.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+
+    @GetMapping("/")
+    public String test() {
+        return "Test";
+    }
+}
